536474f8efa10b561b86f7a9c3e4d8da0f19451a
Cezar Mocan <mocancezar@gmail.com>
2020-05-01 15:43:49 -0400

    Test for twitch embed and chat only

--- pages/home.js
+++ pages/home.js
@@ -6,17 +6,48 @@ import Head from '../components/head'
 
 const sleep = (s) => new Promise((res, rej) => setTimeout(res, s * 1000))
 
+const CHANNEL_NAME = 'mocancezar'
+
 class Home extends React.Component {
+  constructor(props) {
+    super(props)
+    this.streamActive = false
+  }
   static async getInitialProps(ctx) {
     return {
     }
   }
 
   render() {
+    console.log('router: ', this.props.router)
+    const { router } = this.props
+    const chatEmbedSrc = `https://www.twitch.tv/embed/${CHANNEL_NAME}/chat?parent=lipp.tv`
+    
+    if (router.asPath == '/chat-only') {
+      return (
+        <div className="chat-container">
+          <iframe frameborder="0"
+                  scrolling="yes"
+                  id="chat_embed"
+                  src={chatEmbedSrc}
+                  height="500"
+                  width="350">
+          </iframe>
+        </div>
+      )
+    }
+
+    if (router.asPath == '/test-active') {
+      this.streamActive = true
+    }
+
     return (
       <>
         <Head/>
-        <Main/>
+        <Main 
+          streamActive={this.streamActive}
+          channelName={CHANNEL_NAME}
+        />
       </>
     )
   }
