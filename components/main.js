59e5309cd906a67861f8e6de540f118ce48cec67
Cezar Mocan <mocancezar@gmail.com>
2020-05-04 17:12:59 -0400

    half content

--- components/main.js
+++ components/main.js
@@ -87,20 +87,22 @@ class Home extends React.Component {
             <div className="twitch-stream-placeholder section section--no-margin-top" ref={p => this._wlRef = p}>
               <div className="placeholder-text section-item">
                 <h2>
-                  <span class="color--orange">Public access tv</span> 
-                  <span> in</span> the age of <span className="color--green">social distancing</span> 
-                  <span className="color--blue"> is this too much?</span> this is really just a 
-                  <span className="color--yellow"> placeholder</span> for a <span className="color--orange">tagline</span>. 
+                  <span class="color--yellow">Networked television</span> 
+                  <span> in</span> the age of <span className="color--green">social distancing.</span> 
                 </h2>
               </div>
               <div className="placeholder-text section-item">
                 <h2>
-                  LIPP TV will be live<span class="color--yellow"> here </span>
-                  as well as on <a class="color--yellow" href="https://www.twitch.tv/lipp_tv_" target="__blank">TWITCH</a>, <a class="color--yellow">FACEBOOK</a>
-                  <span> &amp; </span><a class="color--yellow">VIMEO</a><br/>
-                  <span class="color--orange">may 11, 2020 8:30 pm</span> eastern time. 
+                  WATCH LIPP TV LIVE<span class="color--orange"> on monday may 11th, 2020 7:30 pm – 10:30 pm</span> eastern time.
+                  TUNE IN TO <span class="color--blue">THIS WEBSITE</span> or watch at <a class="color--yellow" href="https://www.twitch.tv/lipp_tv_" target="__blank">TWITCH</a> and <a className="color--yellow" href="https://www.google.com/url?q=https://www.youtube.com/channel/UCPWF2lJ4E_qVG7HrWRiGnhA/live&sa=D&ust=1588548850999000&usg=AFQjCNE1W4LnlR_wGPZAJq3jqMjSG7dIIw" target="__blank">Youtube</a>.                  
                 </h2>
               </div>
+              <div className="placeholder-text section-item">
+                <h2>
+                <span class="color--yellow">PARTICIPATE LIVE</span> IN THE <span class="color--blue">LIPP TV CHAT</span> BY LOGGING INTO YOUR <span class="color--orange">TWITCH ACCOUNT</span>. IF YOU DON’T HAVE A TWITCH ACCOUNT YOU CAN MAKE ONE <a className="color--yellow" href="https://www.twitch.com">HERE</a> BEFORE THE SHOW.
+                </h2>
+              </div>
+
               <div className="announcement section-item section-item--small-margin">
                 <h4>Scroll down for more details about lipp tv</h4>
               </div>
@@ -127,7 +129,7 @@ class Home extends React.Component {
           <div className="section section--double section--transparent section--align-stretch" ref={p => this._aboutRef = p}>
             <div className="subsection subsection--dark subsection--text">
               <img className="fake-h1" src="img/about.svg"/>
-              <h3 className="light">
+              <h3 className="light spaced-br">
               LIPP TV IS THE FINAL PERFORMANCE PROJECT OF THE LIVE IMAGE PROCESSING AND PERFORMANCE (LIPP) CLASS AT NYU’S ITP. <br/>
 
 LIPP TV IS A CREATIVE RESPONSE TO HOW CODE, VIDEO NETWORKS, AND ART CAN BE USED TO CREATE A NEW EXPERIENCE IN LIVE PERFORMANCE. EACH STUDENT HAS CREATED THEIR OWN SHORT TV SHOW INFLUENCED BY VIDEO ART, EXPERIMENTAL ANIMATION, PUBLIC ACCESS TV, AND MORE. 
@@ -149,21 +151,27 @@ TUNE IN TO LIPP TV AND MAKE SURE TO JOIN THE CHAT AND TALK WITH OUR CREATORS AND
               <h3 className="only-mobile" style={{marginBottom: 20}}>Starting monday, may 11 <br/>at 8:00 pm Eastern time</h3>
 
               <div className="program-section">
-                <div className="program-section-item color--yellow type--bold">8:00 PM – 8:25 PM</div>
-                <div className="program-section-item"> <span className="color--orange type--bold">Lightbox </span>Hosted by Mingna Li </div>
-                <div className="program-section-item"> <span className="color--orange type--bold">Beyond Autopoiesis </span>Hosted by Morgan Mueller </div>
-                <div className="program-section-item"> <span className="color--orange type--bold">Motor Y Cuerda </span>Hosted by David Azar </div>
+                <div className="program-section-item color--yellow type--bold">7:30 PM – 8:00 PM</div>
+              </div>
+              <div className="program-section">
+                <div className="program-section-item color--blue type--bold">INTRODUCTION WITH LIPP TV HOST PROFESSOR LIPP</div>
+              </div>
+              <div className="program-section">
+                <div className="program-section-item"> <span className="color--orange type--bold">STEVE MCQUEEN’S GAME SHOW EXTRAVAGANZA </span>HOSTED BY <a href="http://nickbgrant.com/" className="color--yellow" target="__blank">STEVE MCQUEEN</a></div>
+                <div className="program-section-item"> <span className="color--orange type--bold">MTV JAMS </span>Hosted by <a href="" target="__blank" className="color--yellow">NIKHIL KUMAR</a> </div>
+                <div className="program-section-item"> <span className="color--orange type--bold">NO RESEARCH NATURE </span>Hosted by <a href="" target="__blank" className="color--yellow">BEN MOLL</a> </div>
               </div>
 
               <div className="program-section">
-                <div className="program-section-item color--blue type--bold">INTERMISSION WITH LIPP TV HOST PROFESSOR LIPP</div>
+                <div className="program-section-item color--yellow type--bold">8:00 PM – 8:30 PM</div>
               </div>
-
               <div className="program-section">
-                <div className="program-section-item color--yellow type--bold">8:30 PM – 8:55 PM</div>
-                <div className="program-section-item"> <span className="color--orange type--bold">Amago </span>Hosted by Marcela Mancino </div>
-                <div className="program-section-item"> <span className="color--orange type--bold">Urban Dream </span>Hosted by Jingyi Wen </div>
-                <div className="program-section-item"> <span className="color--orange type--bold">Dad Practice </span>Hosted by August Luhrs </div>
+                <div className="program-section-item color--blue type--bold">SPECIAL GUEST INTERVIEWED BY PROFESSOR LIPP</div>
+              </div>
+              <div className="program-section">
+                <div className="program-section-item"> <span className="color--orange type--bold">STREAM FIGHTER </span>HOSTED BY <a href="http://nickbgrant.com/" target="__blank" className="color--yellow">Christina Dacanay</a> and <a href="http://nickbgrant.com/" target="__blank" className="color--yellow">Cy Kim</a> </div>
+                <div className="program-section-item"> <span className="color--orange type--bold">OFFICE OLYMPICS </span>Hosted by <a href="http://nickbgrant.com/" target="__blank" className="color--yellow">Cezar Mocan</a> </div>
+                <div className="program-section-item"> <span className="color--orange type--bold">RANDOM ACCESS MEMORIES </span>Hosted by <a href="http://nickbgrant.com/" target="__blank" className="color--yellow">SOHAILA MOSBEH</a> and <a href="http://nickbgrant.com/" target="__blank" className="color--yellow">YOUNG MIN CHOI</a> </div>
               </div>
 
               <div className="program-section">
@@ -229,57 +237,53 @@ TUNE IN TO LIPP TV AND MAKE SURE TO JOIN THE CHAT AND TALK WITH OUR CREATORS AND
 
               <div className="program-section">
                 <div style={{marginBottom: 0}} className="program-section-item color--blue type--bold">Matt Romein</div>
-                <div className="program-section-item color--blue">EXECUTIVE PRODUCER, CEO & CCO & CFO</div>
+                <div className="program-section-item color--blue">EXECUTIVE PRODUCER & HOST</div>
               </div>
 
               <div className="program-section">
-                <div style={{marginBottom: 0}} className="program-section-item color--green type--bold">Matt Romein</div>
-                <div className="program-section-item color--green">EXECUTIVE PRODUCER, CEO & CCO & CFO</div>
+                <div style={{marginBottom: 0}} className="program-section-item color--green type--bold">Carrie Wang</div>
+                <div className="program-section-item color--green">CREATIVE DIRECTOR</div>
               </div>
 
               <div className="program-section">
-                <div style={{marginBottom: 0}} className="program-section-item color--blue type--bold">Matt Romein</div>
-                <div className="program-section-item color--blue">EXECUTIVE PRODUCER, CEO & CCO & CFO</div>
+                <div style={{marginBottom: 0}} className="program-section-item color--blue type--bold">Cezar Mocan</div>
+                <div className="program-section-item color--blue">WEB DEVELOPER</div>
               </div>
 
               <div className="program-section">
-                <div style={{marginBottom: 0}} className="program-section-item color--green type--bold">Matt Romein</div>
-                <div className="program-section-item color--green">EXECUTIVE PRODUCER, CEO & CCO & CFO</div>
+                <div style={{marginBottom: 0}} className="program-section-item color--green type--bold">CHRISTINA DACANAY</div>
+                <div className="program-section-item color--green">Graphics and animation</div>
               </div>
 
               <div className="program-section">
-                <div style={{marginBottom: 0}} className="program-section-item color--blue type--bold">Matt Romein</div>
-                <div className="program-section-item color--blue">EXECUTIVE PRODUCER, CEO & CCO & CFO</div>
+                <div style={{marginBottom: 0}} className="program-section-item color--blue type--bold">Caren Ye</div>
+                <div className="program-section-item color--blue">GRAPHICS & ANIMATION</div>
               </div>
 
             </div>
             <div className="subsection subsection--schedule-right">   
               <h3 className="no-mobile" >MEET LIPP TV’S PRODUCTION TEAM<br/> WE’RE NOT JUST MATT ROMEINS</h3>
               <div className="program-section">
-                <div style={{marginBottom: 0}} className="program-section-item color--green type--bold">Matt Romein</div>
-                <div className="program-section-item color--green">EXECUTIVE PRODUCER, CEO & CCO & CFO</div>
+                <div style={{marginBottom: 0}} className="program-section-item color--green type--bold">Aidan Fowler</div>
+                <div className="program-section-item color--green">GRAPHICS & ANIMATION</div>
               </div>
 
               <div className="program-section">
-                <div style={{marginBottom: 0}} className="program-section-item color--blue type--bold">Matt Romein</div>
-                <div className="program-section-item color--blue">EXECUTIVE PRODUCER, CEO & CCO & CFO</div>
+                <div style={{marginBottom: 0}} className="program-section-item color--blue type--bold">Nikhil Kumar</div>
+                <div className="program-section-item color--blue">Music</div>
               </div>
 
               <div className="program-section">
-                <div style={{marginBottom: 0}} className="program-section-item color--green type--bold">Matt Romein</div>
-                <div className="program-section-item color--green">EXECUTIVE PRODUCER, CEO & CCO & CFO</div>
+                <div style={{marginBottom: 0}} className="program-section-item color--green type--bold">SYLVAN ZHENG</div>
+                <div className="program-section-item color--green">Music</div>
               </div>
 
               <div className="program-section">
-                <div style={{marginBottom: 0}} className="program-section-item color--blue type--bold">Matt Romein</div>
-                <div className="program-section-item color--blue">EXECUTIVE PRODUCER, CEO & CCO & CFO</div>
+                <div style={{marginBottom: 0}} className="program-section-item color--blue type--bold">BRENT BAILEY</div>
+                <div className="program-section-item color--blue">Head of advertising</div>
               </div>
 
-              <div className="program-section">
-                <div style={{marginBottom: 0}} className="program-section-item color--green type--bold">Matt Romein</div>
-                <div className="program-section-item color--green">EXECUTIVE PRODUCER, CEO & CCO & CFO</div>
-              </div>
-
+              <h3 className="light" style={{marginTop: '25px'}}>LIPP TV is presented in collaboration with <a className="color--yellow" href="https://culturehub.org" target="__blank">CultureHub</a>.</h3>
             </div>
           </div>
 
