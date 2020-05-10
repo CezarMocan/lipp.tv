fe10d619472a781994bf17359b0946e86a2e634c
Cezar Mocan <mocancezar@gmail.com>
2020-05-10 15:10:59 -0400

    route bugfix

--- pages/home.js
+++ pages/home.js
@@ -6,7 +6,7 @@ import Head from '../components/head'
 
 const sleep = (s) => new Promise((res, rej) => setTimeout(res, s * 1000))
 
-const CHANNEL_NAME = 'mocancezar'
+const CHANNEL_NAME = 'lipp_tv_'
 
 class Home extends React.Component {
   constructor(props) {
@@ -23,21 +23,21 @@ class Home extends React.Component {
     const { router } = this.props
     const chatEmbedSrc = `https://www.twitch.tv/embed/${CHANNEL_NAME}/chat?parent=lipp.tv`
     
-    if (router.asPath == '/chat-only') {
+    if (router.asPath.indexOf('/chat-only') == 0) {
       return (
-        <div className="chat-container">
-          <iframe frameborder="0"
-                  scrolling="yes"
-                  id="chat_embed"
-                  src={chatEmbedSrc}
-                  height="500"
-                  width="350">
-          </iframe>
-        </div>
+          <div className="chat-container" style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100%', width: '100%'}}>
+            <iframe frameborder="0"
+                    scrolling="yes"
+                    id="chat_embed"
+                    src={chatEmbedSrc}
+                    height="600"
+                    width="800">
+            </iframe>
+          </div>
       )
     }
 
-    if (router.asPath == '/active') {
+    if (router.asPath.indexOf('/active') == 0) {
       this.streamActive = true
     }
 
